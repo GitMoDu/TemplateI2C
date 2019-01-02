4fa387f64a8dd21863d4262b35377b19aa76401e
GitMoDu <batatas@gmail.com>
2019-01-02 14:31:37 +0000

    Is now pure virtual, with the message helpers included.

--- src/IMessageI2C.h
+++ src/IMessageI2C.h
@@ -8,11 +8,27 @@
 class IMessageI2C
 {
 public:
-	virtual void Clear() {};
-	virtual uint8_t * GetRaw() { return nullptr; }
-	virtual bool Write(const uint8_t data) { return false; }
-	virtual bool Write(uint8_t* source, const uint8_t length) { return false; }
-	virtual uint8_t GetLength() { return 0; }
+	virtual void Clear();
+	virtual uint8_t * GetRaw();
+	virtual bool Write(const uint8_t data);
+	virtual bool Write(uint8_t* source, const uint8_t length);
+	virtual uint8_t GetLength();
+
+	///Message building helpers.
+	virtual uint8_t GetHeader();
+	virtual void SetHeader(const uint8_t header);
+
+	virtual uint16_t Get8BitPayload(const uint8_t offset = 0);
+	virtual uint16_t Get16BitPayload(const uint8_t offset = 0);
+	virtual uint32_t Get32BitPayload(const uint8_t offset = 0);
+
+	virtual bool Append8BitPayload(const uint8_t value);
+	virtual bool Append16BitPayload(const uint16_t value);
+	virtual bool Append32BitPayload(const uint32_t value);
+
+	virtual bool Set8BitPayload(const uint8_t value, const uint8_t offset = 0);
+	virtual bool Set16BitPayload(const uint16_t value, const uint8_t offset = 0);
+	virtual bool Set32BitPayload(const uint32_t value, const uint8_t offset = 0);
 };
 
 #endif
