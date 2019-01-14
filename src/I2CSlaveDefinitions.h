17c4b3cb302d130082be2eb7cb6f17708b81dfa9
GitMoDu <batatas@gmail.com>
2019-01-14 14:16:59 +0000

    Updated comments.

--- src/I2CSlaveDefinitions.h
+++ src/I2CSlaveDefinitions.h
@@ -3,20 +3,21 @@
 #ifndef _I2CSLAVEDEFINITIONS_h
 #define _I2CSLAVEDEFINITIONS_h
 
-//This feature is only useful if you have rom/ram to spare and want a clean way to composite a message class.
+//This feature is only useful if you have ROM to spare and want a clean way to composite a message class,
+// or just save RAM with variable size template message.
 //Otherwise, it's best turned off, as it simplifies the class object into a simples intance with no virtual calls.
 //#define I2C_MESSAGE_IMPLEMENT_INTERFACE
 
 //Basic messaging error tracking is nice, but it can be disabled with this property.
 #define I2C_SLAVE_COMMS_ERRORS_ENABLE
 
-////If you don't use a task scheduller, you need to manually the callback whenever possible.
+//If you don't use a task scheduller, you need to manually the callback whenever possible.
 #define I2C_SLAVE_USE_TASK_SCHEDULER
 
 //Enable device reset.
 #define I2C_SLAVE_DEVICE_RESET_ENABLE
 
-//Enable device health messages. Requires I2C_MESSAGE_IMPLEMENT_INTERFACE enabled.
+//Enable device health messages.
 #define I2C_SLAVE_HEALTH_ENABLE
 
 #endif
