a2f4d49e4aed58c8a2e6fff8f0c62ecc8f47b622
GitMoDu <batatas@gmail.com>
2024-11-21 18:58:14 +0000

    New working version, without async.

---
+++ src/TemplateI2cDriver.h
@@ -0,0 +1,8 @@
+// TemplateI2cDriver.h
+
+#ifndef _TEMPLATE_I2C_DRIVER_INCLUDE_h
+#define _TEMPLATE_I2C_DRIVER_INCLUDE_h
+
+#include "Driver/TemplateI2cDriver.h"
+
+#endif
---
+++ src/TemplateI2cSlave.h
@@ -0,0 +1,8 @@
+// TemplateI2cSlave.h
+
+#ifndef _TEMPLATE_I2C_SLAVE_INCLUDE_h
+#define _TEMPLATE_I2C_SLAVE_INCLUDE_h
+
+#include "Slave/TemplateI2cSlave.h"
+
+#endif
