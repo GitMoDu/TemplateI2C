733c25b0cfc6343cb48f333162cd154f5ab967b6
GitMoDu <batatas@gmail.com>
2024-11-21 18:55:29 +0000

    Temporarily remove files to force correct case.

--- src/TemplateI2CDriver.h
+++
@@ -1,8 +0,0 @@
-// TemplateI2cDriver.h
-
-#ifndef _TEMPLATE_I2C_DRIVER_INCLUDE_h
-#define _TEMPLATE_I2C_DRIVER_INCLUDE_h
-
-#include "Driver/TemplateI2cDriver.h"
-
-#endif
--- src/TemplateI2CSlave.h
+++
@@ -1,8 +0,0 @@
-// TemplateI2cSlave.h
-
-#ifndef _TEMPLATE_I2C_SLAVE_INCLUDE_h
-#define _TEMPLATE_I2C_SLAVE_INCLUDE_h
-
-#include "Slave/TemplateI2cSlave.h"
-
-#endif
